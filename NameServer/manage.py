4193b58f9118b00f94216e7f6e17f2823e12c648
Andrea Gulino <andrea.gulino@polimi.it>
2019-11-30 22:18:00 +0100

    Update manage.py

--- NameServer/manage.py
+++ NameServer/manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import sys
 
 if __name__ == "__main__":
     os.environ["DJANGO_SETTINGS_MODULE"] = 'NameServer.settings'
@@ -19,3 +20,4 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
+    execute_from_command_line(sys.argv)
