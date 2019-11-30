325d08c995bd0fee91641d64e3a319f05893c426
Andrea Gulino <andrea.gulino@polimi.it>
2019-11-30 20:45:16 +0100

    Update settings.py

--- NameServer/NameServer/settings.py
+++ NameServer/NameServer/settings.py
@@ -46,9 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    'background_task'
+    'django.contrib.staticfiles'
 ]
 
 REST_FRAMEWORK = {
